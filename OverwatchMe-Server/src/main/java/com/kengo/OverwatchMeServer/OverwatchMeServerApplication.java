a9861ff960d29575122e90dea5c5fa017a50fa0b
Kengo <kengo@binner.org>
2020-02-13 21:00:55 +0000

    Creation of OverwatchMe Server

---
+++ OverwatchMe-Server/src/main/java/com/kengo/OverwatchMeServer/OverwatchMeServerApplication.java
@@ -0,0 +1,13 @@
+package com.kengo.OverwatchMeServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverwatchMeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OverwatchMeServerApplication.class, args);
+	}
+
+}
---
+++ OverwatchMe-Server/src/test/java/com/kengo/OverwatchMeServer/OverwatchMeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kengo.OverwatchMeServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OverwatchMeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
