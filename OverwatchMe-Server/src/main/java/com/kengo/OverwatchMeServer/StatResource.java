8eb5ce9abec1ffc5c2da98de789e2c1c6354c150
Kengo <kengo@binner.org>
2020-02-15 01:00:09 +0000

    Get Method working for psn + xbl, not for battlenet

--- OverwatchMe-Server/src/main/java/com/kengo/OverwatchMeServer/StatResource.java
+++ OverwatchMe-Server/src/main/java/com/kengo/OverwatchMeServer/StatResource.java
@@ -1,28 +1,41 @@
 package com.kengo.OverwatchMeServer;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.*;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.http.HttpHeaders;
 
 @RestController
+@RequestMapping
 public class StatResource {
 
-    @RequestMapping("/stats/{playerId}")
-    public ResponseEntity<String> getStats(@PathVariable("playerId") String playerId){
+    public String apiUrl = "https://public-api.tracker.gg/v2/overwatch/standard/profile/";
+    public String apiKey = "b92f295f-b1be-40ce-8b48-fc080fedf90d";
+
+    //Works for psn and xbl, not for battlenet
+
+    @GetMapping(value="api/{platform}/{playerId}")
+    public ResponseEntity<String> getStats(@PathVariable("platform") String platform,
+                                           @PathVariable("playerId") String playerId){
         RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("TRN-Api-Key", "b92f295f-b1be-40ce-8b48-fc080fedf90d");
 
         String stats = restTemplate.getForObject(
-                "https://public-api.tracker.gg/v2/overwatch/standard/profile/battlenet/" + playerId + "%232772",
-                String.class
-        );
+                apiUrl + platform + "/" + playerId +"?TRN-Api-Key=" + apiKey, String.class);
 
         return new ResponseEntity<String>(stats, HttpStatus.OK);
     }
+
+    //Working Apex Get Method
+    /*
+    public String apexUrl = "https://public-api.tracker.gg/v2/apex/standard/profile/";
+    @GetMapping(value="api/{platform}/{playerId}")
+    public ResponseEntity<String> getStats(@PathVariable("platform") String platform,
+                                           @PathVariable("playerId") String playerId){
+        RestTemplate restTemplate = new RestTemplate();
+        String stats = restTemplate.getForObject(
+                apexUrl + platform + "/" + playerId + "?TRN-Api-Key=" + apiKey, String.class);
+
+        return new ResponseEntity<String>(stats, HttpStatus.OK);
+    }*/
 }
